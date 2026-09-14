8beb61f8598d27d620e5d20eb3d6868943c7a2b1
David Mariscal Fernández <david.mariscal.fernandez.09@gmail.com>
2026-09-14 13:59:22 +0200

    Pin OCI deployment to latest verified infrastructure

--- compute.tf
+++ compute.tf
@@ -10,7 +10,7 @@ data "oci_core_images" "ubuntu" {
 locals {
   api_site           = trimspace(var.api_hostname) == "" ? ":80" : trimspace(var.api_hostname)
   ssh_enabled        = trimspace(var.ssh_public_key) != ""
-  verified_infra_ref = "4f7e8796568b652f88e856a337352c36abfbae75"
+  verified_infra_ref = "852c0238b00a40ee8ae6942fb773083c5d248d38"
 }
 
 resource "oci_core_instance" "crisisweave" {
